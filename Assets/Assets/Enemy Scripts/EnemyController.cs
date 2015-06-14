471439fe5f6b98b99da715081918e7ebe9c52903
Ssteve85uk <stephen.mangold@knights.ucf.edu>
2015-06-14 09:19:13 -0400

    Updated to Tylers Version
    updated

---
+++ Assets/Assets/Enemy Scripts/EnemyController.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour
+{	//set up variables and get controller
+	public float health;
+	public SingerAI Singer;
+	public DrummerAI Drummer;
+	public GuitarAI Guitar;
+	public DestObj Dest;
+	public Vector3 playerPosition;
+	public bool isRotated;
+	public float dropItem;
+	public GameObject HPdrop;
+	public GameObject spark;
+//	Vector3 movement = Vector3.zero;
+		// Use this for initialization
+		void Start ()
+		{
+		//access to controller and other connected scripts
+		Singer = GetComponent<SingerAI> ();
+		Drummer = GetComponent<DrummerAI> ();
+		Guitar = GetComponent<GuitarAI> ();
+		Dest = GetComponent<DestObj> (); 
+		Dest.enabled = false;
+		isRotated = false;
+		// starts health check in 2 seconds and to repeat every second after
+		InvokeRepeating ("CheckHealth", 3, 1);
+		// test damage
+		//InvokeRepeating ("TestDamage", 5, 5);
+		//controller = GetComponent<CharacterController> ();
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+		//Flipping to face player
+		playerPosition = (GameObject.Find ("Player").transform.position);
+		if (playerPosition.x > transform.position.x && isRotated == false) {
+			transform.Rotate(0, 180, 0); 
+			isRotated = true;
+		}
+		if (playerPosition.x < transform.position.x && isRotated == true) {
+			transform.Rotate(0, 180, 0); 
+			isRotated = false;
+		}
+		/*if (controller.isGrounded == false)
+						movement.y += Physics.gravity.y * Time.deltaTime;
+				
+		controller.Move (movement * Time.deltaTime);
+		*/
+		}
+	 void CheckHealth(){	
+		// kill if health hits zero
+		if (health < 1) {
+			Dest.enabled = true;
+			dropItem = Random.Range(1,9);
+			if (dropItem >5){
+				//drop health
+				Instantiate(HPdrop, transform.position, transform.rotation);
+			}
+		}
+	} 
+	void TestDamage(){
+		health--;
+		}
+	public void GetHit(float dmg){
+		health -= dmg;
+		Instantiate(spark, transform.position, transform.rotation);
+	}
+	void OnTriggerEnter(Collider c){
+		Vector3 PlayerPos = c.transform.position;
+		if (c.gameObject.tag == "Player") {
+			if(PlayerPos.x < transform.position.x){
+			c.attachedRigidbody.AddForce(new Vector3(1 * -1, 1, 0) * 100);
+			}
+			else if(PlayerPos.x > transform.position.x){
+				c.attachedRigidbody.AddForce(new Vector3(1 * 1, 1, 0) * 100);
+			}
+			if(PlayerPos.y < transform.position.y){
+				c.attachedRigidbody.AddForce(new Vector3(1 * -1, 1, 0) * 100);
+			}
+			else if(PlayerPos.y > transform.position.y){
+				c.attachedRigidbody.AddForce(new Vector3(1 * 1, 1, 0) * 100);
+			}
+		}
+
+	}
+}
+
---
+++ Assets/Assets/Scripts/AttackAI.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackAI : MonoBehaviour {
+	public float speed;
+	public float maxDist;
+	public float power;
+	public float startX, startY, startZ;
+	public GameObject p;
+	public MoveTest pMove;
+	public PlayerAttack pAttack;
+	public int dir;
+	// Use this for initialization
+	void Start () {
+		maxDist = 1.0f;
+		startX = transform.position.x;
+		startY = transform.position.y;
+		startZ = transform.position.z;
+		p = GameObject.Find ("Player");
+		pMove = p.GetComponent<MoveTest> ();
+		pAttack = p.GetComponent<PlayerAttack> ();
+
+		if (pMove.isRight == 1)
+			dir = 1;
+		if (pMove.isRight == -1)
+			dir = -1;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (dir == 1) {
+			if (pAttack.combo == 3)
+				maxDist = 1.5f;
+			if (transform.position.x < (startX + maxDist)) {
+				transform.position = new Vector3 (transform.position.x + (speed * Time.deltaTime), startY, startZ);
+			}
+		}
+		else {
+			if (pAttack.combo == 3)
+				maxDist = 1.5f;
+			if (transform.position.x > (startX - maxDist)) {
+				transform.position = new Vector3 (transform.position.x - (speed * Time.deltaTime), startY, startZ);
+			}
+		}
+		if (transform.position.x < p.transform.position.x + 1.5f) {
+			//transform.position = new Vector3 (p.transform.position.x + 2.5f, transform.position.y, transform.position.z);
+		}
+	}
+
+	void OnTriggerEnter(Collider c){
+		if (c.gameObject.tag == "Enemy") {
+			if (pAttack.combo == 3){
+				c.attachedRigidbody.AddForce(new Vector3(1 * dir, 1, 0) * 200);
+			}
+			else {
+				c.attachedRigidbody.AddForce(new Vector3(1 * dir, 1, 0) * 80);
+			}
+
+			c.gameObject.GetComponent<EnemyController>().GetHit (power);
+		}
+	}
+}
