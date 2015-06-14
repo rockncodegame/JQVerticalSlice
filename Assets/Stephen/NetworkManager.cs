d3db473fe0ecd8e1a054a1e9ffb1b6a3a535788b
Ssteve85uk <stephen.mangold@knights.ucf.edu>
2015-06-14 02:22:39 -0400

    network
    t

---
+++ Assets/Stephen/NetworkManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+	private const string typeName = "UniqueGameName";
+	private const string gameName = "RoomName";
+	private HostData[] hostList;
+	private void StartServer()
+	{
+		Network.InitializeServer(4, 25000, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName, gameName);
+	}
+	void OnServerInitialized()
+	{
+		Debug.Log("Server Initializied");
+	}
+	private void RefreshHostList()
+	{
+		MasterServer.RequestHostList(typeName);
+	}
+	
+	void OnMasterServerEvent(MasterServerEvent msEvent)
+	{
+		if (msEvent == MasterServerEvent.HostListReceived)
+			hostList = MasterServer.PollHostList();
+	}
+	void OnGUI()
+	{
+		if (!Network.isClient && !Network.isServer)
+		{
+			if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+				StartServer();
+			
+			if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
+				RefreshHostList();
+			
+			if (hostList != null)
+			{
+				for (int i = 0; i < hostList.Length; i++)
+				{
+					if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+						JoinServer(hostList[i]);
+				}
+			}
+		}
+	}
+	private void JoinServer(HostData hostData)
+	{
+		Network.Connect(hostData);
+	}
+	
+	void OnConnectedToServer()
+	{
+		Debug.Log("Server Joined");
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
