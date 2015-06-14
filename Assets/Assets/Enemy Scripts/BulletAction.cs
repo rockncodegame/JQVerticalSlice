8c005fb10aab44722b5e00beef56951d1db92f26
ewatkins2010 <eddiewattz@gmail.com>
2015-06-14 14:18:02 -0400

    collision
    added enemy bullet collision

--- Assets/Assets/Enemy Scripts/BulletAction.cs
+++ Assets/Assets/Enemy Scripts/BulletAction.cs
@@ -4,19 +4,36 @@ using System.Collections;
 public class BulletAction : MonoBehaviour {
 	//public int shotType;
 	//public int speed;
-	public Vector3 playerPosition;
+	public GameObject p;
+	public MoveTest pMove;
+	public PlayerStats pStats;
+	public float speed;
+	//public Vector3 playerPosition;
 	// Use this for initialization
 	void Start () {
+		speed = 8 * Time.deltaTime;
 		//set bullet to auto destroy after a time
 		Destroy (gameObject, 0.5f);
-		playerPosition = (GameObject.Find ("Player").transform.position);
+		p = GameObject.Find ("Player");
+		pMove = p.GetComponent<MoveTest> ();
+		pStats = p.GetComponent<PlayerStats> ();
 	}
-	
 
 	// Update is called once per frame
 	void Update () {
-		float speed = 8 * Time.deltaTime;
-		transform.position = Vector3.MoveTowards (transform.position, playerPosition, speed);
+		transform.position = Vector3.MoveTowards (transform.position, p.transform.position, speed);
+	}
+
+	void OnTriggerEnter(Collider c){
+		if (c.gameObject.tag == "Player") {
+			if (transform.position.x <= p.transform.position.x)
+				pMove.movement = new Vector3(130.0f, 150.0f, 0.0f) * Time.deltaTime;
+			else
+				pMove.movement =  new Vector3(-130.0f, 150.0f, 0.0f) * Time.deltaTime;
+			
+			pMove.stun = Time.time + .5f;
+			pStats.GetHit(1);
+		}
 	}
 	
 }
