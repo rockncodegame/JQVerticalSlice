27a0710cc9c53d19baadf4947dcc5a1a987a13fe
ewatkins2010 <eddiewattz@gmail.com>
2015-06-18 00:36:19 -0400

    Added a zoom effect
    Camera now zooms in when player is in the back plane

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,20 +3,33 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 	public GameObject p;
-	public bool isLocked, reset;
-	public Vector3 pPos;
+	public bool isLocked, reset, inBuilding;
+	public Vector3 pPos, pPosZoom, camInitial;
+	public float zoom, startZ;
 	// Use this for initialization
 	void Start () {
 		p = GameObject.Find ("Player");
 		isLocked = false;
 		reset = false;
-
+		inBuilding = false;
+		startZ = transform.position.z;
+		zoom = p.transform.position.z - 2;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		pPos = new Vector3 (p.transform.position.x, transform.position.y, transform.position.z);
+		pPosZoom = new Vector3 (p.transform.position.x, transform.position.y, zoom);
+		camInitial = new Vector4 (p.transform.position.x, transform.position.y, startZ);
 		if (!isLocked)
-			transform.position = pPos;;
+			transform.position = pPos;
+		if (inBuilding){
+			if (transform.position.z != zoom)
+				transform.position = Vector3.MoveTowards(transform.position, pPosZoom, .1f);
+		}
+		else {
+			if (transform.position.z != startZ)
+				transform.position = Vector3.MoveTowards(transform.position, camInitial, .1f);
+		}
 	}
 }
---
+++ Assets/Scripts/StageZoom.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageZoom : MonoBehaviour {
+	public GameObject cam;
+	public CameraFollow cFollow;
+	// Use this for initialization
+	void Start () {
+		cam = GameObject.Find ("Main Camera");
+		cFollow = cam.GetComponent<CameraFollow> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider c){
+		if (c.gameObject.tag == "Player")
+			cFollow.inBuilding = true;
+	}
+	void OnTriggerExit(Collider c) {
+		if (c.gameObject.tag == "Player")
+			cFollow.inBuilding = false;
+	}
+}
