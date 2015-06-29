646b5782be69b5a18037955bb40ad365ad02644d
kopyright13 <Vault@Hezekiahs-MacBook-Pro-3.local>
2015-06-29 12:01:38 -0400

    Polish

---
+++ Assets/Scripts/Level_Transition.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level_Transition : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		Application.LoadLevel("mainmenu");
+	}
+
+}
