1d3cb5cc9f3f4e6740879638685f0c55745a8ca6
kopyright13 <Vault@Hezekiahs-MacBook-Pro-2.local>
2015-07-17 14:35:10 -0400

    Updates

---
+++ Assets/Scripts/Music_Trigger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music_Trigger : MonoBehaviour {
+
+	// Use this for initialization
+	public AudioSource Track1;
+	public AudioSource Track2;
+
+	void Start () {
+		Track1.Play ();
+		Track2.Pause ();
+	}
+
+	void OnTriggerEnter(Collider Gobject)
+	{
+		if(Gobject.tag == "Player")
+		{
+			Track1.Pause();
+			Track2.Play();
+		}
+	}
+
+	void OnTriggerExit(){
+
+			Track2.Pause();
+			Track1.Play();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Music_Trigger1.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music_Trigger1 : MonoBehaviour {
+
+	// Use this for initialization
+	public AudioSource Track3;
+
+	void Start () {
+		Track3.Stop ();
+	}
+
+	void OnTriggerEnter(Collider Gobject)
+	{
+		if(Gobject.tag == "Player")
+		{
+			Track3.Play();
+		}
+	}
+
+	void OnTriggerExit(){
+
+
+			Track3.Stop();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
