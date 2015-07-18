c5f1aaafef80ed414c4e2a5539595716e4b095d5
kopyright13 <Vault@Hezekiahs-MacBook-Pro-2.local>
2015-07-18 19:29:12 -0400

    Level 2 and 3 update

---
+++ Assets/Scripts/NextLevel1.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevel1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnTriggerEnter(Collider other) 
+	{
+		Application.LoadLevel ("Level 3");
+	}
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
