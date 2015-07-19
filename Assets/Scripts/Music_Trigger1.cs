1c1b0467791d403f53ec61a3f6f53830679667ba
kopyright13 <Vault@Hezekiahs-MacBook-Pro-2.local>
2015-07-19 18:13:54 -0400

    level 3

--- Assets/Scripts/Music_Trigger1.cs
+++ Assets/Scripts/Music_Trigger1.cs
@@ -5,23 +5,28 @@ public class Music_Trigger1 : MonoBehaviour {
 
 	// Use this for initialization
 	public AudioSource Track3;
+	public AudioSource Track4;
+	public float volume;
 
 	void Start () {
-		Track3.Stop ();
+		volume = 0.0f;
+		Track3.Play();
+		Track3.volume = volume;
+		Track4.volume = 1.0f;
 	}
 
 	void OnTriggerEnter(Collider Gobject)
 	{
 		if(Gobject.tag == "Player")
 		{
-			Track3.Play();
+			Track3.volume = 1.0f;
+			Track4.volume = 0.0f;
 		}
 	}
 
 	void OnTriggerExit(){
-
-
-			Track3.Stop();
+		Track3.volume = 0.0f;
+		Track4.volume = 1.0f;
 	}
 
 	// Update is called once per frame
