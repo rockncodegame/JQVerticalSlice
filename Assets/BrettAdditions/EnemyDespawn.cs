4e843b9aefd77bd095923e2ef13d1a26252bcf80
brettdrichards <brettdrichards423@gmail.com>
2015-07-24 14:51:50 -0400

    enemy despawn & spawners w/ activate
    w/ activate enemy & despawn spawners

---
+++ Assets/BrettAdditions/EnemyDespawn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDespawn : MonoBehaviour {
+
+	public GameObject enemy;
+	public GameObject p;
+	public float despawnDistance;
+	
+	// Use this for initialization
+	void Start () {
+		p = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((p.transform.position.x - enemy.transform.position.x > despawnDistance) || (p.transform.position.x - enemy.transform.position.x < (despawnDistance*-1))) {
+			Destroy(enemy);
+		}
+	}
+}
---
+++ Assets/BrettAdditions/SpawnDeactivate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnDeactivate : MonoBehaviour {
+
+	public VisSpawnPrefabTrigger spawner;
+	public GameObject p;
+	public GameObject spawnerPosition;
+	public float deactivateDistance;
+	
+	// Use this for initialization
+	void Start () {
+		spawner.GetComponentInParent<VisSpawnPrefabTrigger> ();
+		p = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((p.transform.position.x - spawnerPosition.transform.position.x > deactivateDistance) || (p.transform.position.x - spawnerPosition.transform.position.x < (deactivateDistance * -1))) {
+			spawner.enabled = false;
+		} else {
+			spawner.enabled = true;
+		}
+	}
+}
