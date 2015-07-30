90da600c14b25401957dc16f46232919e5e99b82
ewatkins2010 <eddiewattz@gmail.com>
2015-07-30 00:54:26 -0400

    Menu & Attack Stuffs
    Pause menu navigation
    Speed up/Slow down transition sound
    Ultra attack added

--- Assets/Scripts/Object Code/pause.cs
+++ Assets/Scripts/Object Code/pause.cs
@@ -1,18 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class pause : MonoBehaviour {	
-	public bool isPaused;
+	public bool isPaused, canChange;
 	public GameObject PauseMenu;
+	public Button[] btns;
+	public int selectedIndex;
+	public float cHorizontal;
+
 	void Start() {
 		isPaused = false;
 		PauseMenu = GameObject.Find ("Pause Menu");
 		PauseMenu.SetActive (false);
+		isPaused = false;
+		selectedIndex = 0;
+		canChange = true;
 	}
 
 	void Update () {
+		cHorizontal = Mathf.Round (Input.GetAxis ("Horizontal"));
 		if ((Input.GetKeyDown (KeyCode.Escape) || Input.GetKeyDown (KeyCode.JoystickButton7))  && !isPaused)
 			PauseGame ();
+
+		if (Input.GetKeyDown (KeyCode.RightArrow) || cHorizontal > 0) {
+			if(canChange && isPaused){
+				selectedIndex++;
+				canChange = false;
+			}
+		}
+				
+		if (Input.GetKeyDown (KeyCode.LeftArrow) || cHorizontal < 0) {
+			if(canChange && isPaused){
+				selectedIndex--;
+				canChange = false;
+			}
+		}
+
+		if (cHorizontal == 0)
+			canChange = true;
+		
+		if (selectedIndex < 0)
+			selectedIndex = 2;
+
+		if (selectedIndex > 2)
+			selectedIndex = 0;
+
+		if (isPaused)
+			HighlightBtn ();
+	}
+
+	public void HighlightBtn() {
+		switch (selectedIndex){
+		case 0:
+			btns[0].Select ();
+			if (Input.GetKeyDown (KeyCode.Return) || Input.GetKeyDown (KeyCode.JoystickButton0))
+				ResumeGame ();
+			break;
+		case 1:
+			btns[1].Select ();
+			if (Input.GetKeyDown (KeyCode.Return) || Input.GetKeyDown (KeyCode.JoystickButton0))
+				MainMenu ();
+			break;
+		case 2:
+			btns[2].Select ();
+			if (Input.GetKeyDown (KeyCode.Return) || Input.GetKeyDown (KeyCode.JoystickButton0))
+				QuitGame ();
+			break;
+		}
 	}
 
 	public void PauseGame(){
